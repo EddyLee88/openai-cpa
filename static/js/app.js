44339c6f6c3cd90f55a5d3f933c86e39f77f3238
“文风轩灵” <467884979@qq.com>
2026-04-20 14:12:46 +0800

    update

--- static/js/app.js
+++ static/js/app.js
@@ -22,7 +22,7 @@ function normalizeBooleanLike(value, defaultValue = false) {
 createApp({
     data() {
         return {
-            appVersion: 'v11.0.3',
+            appVersion: 'v11.1.0',
             isLoggedIn: !!localStorage.getItem('auth_token'),
             loginPassword: '',
             currentTab: window.location.hash.replace('#', '') || 'console',
